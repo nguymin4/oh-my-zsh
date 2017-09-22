82ccca4e34de4c616e273cdef97d6ae20d8ff5b3
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2017-09-22 03:21:23 +0300

    Modify history.zsh

--- lib/history.zsh
+++ lib/history.zsh
@@ -40,9 +40,15 @@ esac
 [ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
 
 ## History command configuration
-setopt extended_history       # record timestamp of command in HISTFILE
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
-setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
-setopt share_history          # share command history data
+setopt append_history
+setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
+setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
+setopt hist_find_no_dups         # Do not display a line previously found.
+setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
+setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
+setopt hist_ignore_space         # Ignore commands that start with space
+setopt hist_reduce_blanks        # Remove superfluous blanks before recording entry.
+setopt hist_save_no_dups         # Don't write duplicate entries in the history file.
+setopt hist_verify               # Don't execute immediately upon history expansion.
+setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
+setopt share_history             # Share history between all sessions.
