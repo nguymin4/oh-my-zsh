bb20bbf8cc9376e2861916349d4d7d7061f5ee66
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-08-26 16:26:51 +0300

    feat: update check-font-styles function

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -10,7 +10,8 @@ check-font-styles() {
   echo -e "\e[3mitalic\e[0m"
   echo -e "\e[3m\e[1mbold italic\e[0m"
   echo -e "\e[4munderline\e[0m"
+  echo -e "\e[4:3mundercurl\e[m"
   echo -e "\e[9mstrikethrough\e[0m"
-  echo -e "\e[31mHello World\e[0m"
-  echo -e "\x1B[31mHello World\e[0m"
+  echo -e "\e[31mred\e[0m"
+  echo -e "\x1B[31mred\e[0m"
 }
