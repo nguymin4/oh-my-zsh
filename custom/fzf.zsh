096cad72938bae7ee9ff27eaa6c85175c9ed2e49
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-12-28 14:43:14 +0200

    feat: rework theme change function
    Support live updating for neovim and simplify other configs

--- custom/fzf.zsh
+++ custom/fzf.zsh
@@ -1,13 +1,20 @@
-if [ -f $HOME/.fzf.zsh ]; then
-  source $HOME/.fzf.zsh
+set_fzf_theme() {
+  local fzf_theme='light'
 
-  COLOR_BG='dark'
-  if ! grep -Fq 'colors: *dark' $HOME/.config/alacritty/alacritty.yml; then
-    COLOR_BG='light'
+  if [[ -n "$1" ]]; then
+    fzf_theme="$1"
+  else
+    if grep -Fq 'edge-dark.vim' $HOME/.vimrc; then
+      fzf_theme='dark'
+    fi
   fi
 
-  export FZF_DEFAULT_OPTS='--height 25%'
-  export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS" --color=${COLOR_BG},hl:2,hl+:1,bg+:0,gutter:0,info:3,prompt:4,pointer:1"
+  export FZF_DEFAULT_OPTS="--height 25% --color=${fzf_theme},bg:-1,bg+:-1,hl:2,hl+:1,pointer:1,info:3"
+}
+
+if [ -f $HOME/.fzf.zsh ]; then
+  source $HOME/.fzf.zsh
+  set_fzf_theme
 fi
 
 _fzf_compgen_path() {
